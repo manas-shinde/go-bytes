fdb35f6eb63d0cdd6c189af84989d6c0e5db343d
manas-shinde <manas.a.shinde11@gmail.com>
2025-07-31 06:34:59 +0530

    feat: closure in golang.

--- cmd/tutorial_2_functions/main.go
+++ cmd/tutorial_2_functions/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	var num int = 10
 	var den int = 3
-	var result, remainder, err = intDivision(num, den)
+	var result, remainder, err = intDivision(num, den, printMe)
 	if err != nil {
 		fmt.Println("Error:", err.Error())
 	} else if remainder == 0 {
@@ -30,13 +30,29 @@ func main() {
 	// 	fmt.Printf("Result of %d / 0 = %d and remainder is %d\n", num, res, rem)
 	// }
 
+	// Closure example
+	fmt.Println("Closure example:")
+	pos, neg := adder(), adder()
+	for i := 0; i < 5; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+
+	// Fibonacci example
+	fmt.Println("Fibonacci example (using closure):")
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
 
 func printMe(printValue string) {
 	fmt.Println(printValue)
 }
 
-func intDivision(num int, den int) (int, int, error) {
+func intDivision(num int, den int, printMe func(string)) (int, int, error) {
 	var error error = nil
 	if den == 0 {
 		error = errors.New("Division by zero error")
@@ -45,5 +61,28 @@ func intDivision(num int, den int) (int, int, error) {
 	var result int = num / den
 	var remainder int = num % den
 
+	// Functions are values too. They can be passed around just like other values.
+	printMe("Inside intDivision function")
+
 	return result, remainder, error
+}
+
+// Closure in Go
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		ret := a
+		a, b = b, a+b
+		return ret
+	}
 }
